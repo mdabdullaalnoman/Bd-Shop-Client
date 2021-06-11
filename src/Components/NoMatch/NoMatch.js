88420adbd02259daa50ce2f809d995ccad152d4e
mdabdullaalnoman <noman.apparel.4003@gmail.com>
2021-06-11 16:11:15 +0600

    start journoy

---
+++ src/Components/NoMatch/NoMatch.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NoMatch = () => {
+      return (
+            <div>
+                  <h1>this is nomatch</h1>
+            </div>
+      );
+};
+
+export default NoMatch;
